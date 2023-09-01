c400dcb9a0936d9e3fcd7125d45a7942292ce46f
jzinno <johnzinno14@gmail.com>
2023-09-01 11:47:07 -0400

    add optimized fused simulation

--- src/lib.rs
+++ src/lib.rs
@@ -66,10 +66,48 @@ fn ltt_auc(stem_cell_size: usize, generation_time: u64) -> f64 {
 }
 
 
+#[pyfunction]
+fn fused_ltt_auc(stem_cell_size: usize, generation_time: u64) -> f64{
+    let max_generations = 10000;
+    let ancestors: Vec<i64> = (0..stem_cell_size as i64).collect();
+
+    let mut pop = ancestors.clone();
+
+    let mut rng = rand::thread_rng();
+
+    let mut unique_cells: HashSet<i64> = HashSet::with_capacity(stem_cell_size);
+    let mut lineages = Vec::with_capacity(max_generations);
+    let mut time_vector = Vec::with_capacity(max_generations);
+    
+    let mut current_gen = 0;
+    while current_gen < max_generations {
+        unique_cells.extend(&pop);
+        lineages.push(unique_cells.len());
+        time_vector.push(current_gen as u64 * generation_time);
+        
+        if unique_cells.len() == 1 {
+            break;
+        }
+
+        pop = (0..stem_cell_size)
+            .map(|_| *pop.choose(&mut rng).unwrap())
+            .collect();
+
+        current_gen += 1;
+        unique_cells.clear();
+    }
+
+    auc(&time_vector, &lineages)
+}
+
+
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn wfrs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(wright_fisher_sim, m)?)?;
     m.add_function(wrap_pyfunction!(ltt_auc, m)?)?;
+    m.add_function(wrap_pyfunction!(fused_ltt_auc, m)?)?;
     Ok(())
 }
